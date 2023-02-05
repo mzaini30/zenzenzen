0ca3bca275f9a4f6270f4b9b9a6f665e3e102618
Zen <muhzaini30@gmail.com>
2023-02-05 20:41:09 +0800

    sudah sistem komentar

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,10 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), sitemap()],
-  site: "https://zenzen.web.id",
+  integrations: [tailwind(), mdx(), sitemap(), svelte()],
+  site: "https://zenzen.web.id"
 });
---
+++ src/data/api.js
@@ -0,0 +1,3 @@
+export const sql = "https://excalibur.jaddid.my.id/sql.php";
+
+export const komentar = "c6e3894998f0bc7de76108e83437e7dc"; // id, nama, email, komentar, idnya
