21c85647c3a9e686c7f962a603a510e294d9d86f
mzaini30 <mzaini30@users.noreply.github.com>
2023-02-07 08:45:57 +0000

    deploy: 807973bc633147e3b44fe685da64f09a3f119a85

--- _astro/hoisted.c3f99458.js
+++
@@ -1,1 +0,0 @@
-import"./post.astro_astro_type_script_index_0_lang.59092d30.js";
--- _astro/post.astro_astro_type_script_index_0_lang.59092d30.js
+++
@@ -1,1 +0,0 @@
-const t=document.querySelector(".tombol-cari-post");t.addEventListener("click",()=>{const o=prompt("Cari apa?");o&&(window.location.href=`/cari?${encodeURIComponent(o)}`)});
