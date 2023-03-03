89eea4f2a2f9dc85a2061f34060cd2281cc1be8e
mzaini30 <muhzaini30@gmail.com>
2023-03-03 14:31:24 +0800

    simpan dulu

--- baru.mjs
+++ baru.mjs
@@ -3,16 +3,22 @@ import {writeFileSync} from "fs"
 import {slug} from "kumpulan-tools"
 
 const judul = await text("Masukkan judul:")
+const d = new Date()
+
+// 2023-02-01 09:01:00
+// const tanggal = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`
+// untuk yang bulan, tanggal, jam, menit, detik; jangan lupa dikasih 0 di depannya jika angkanya cuma satuan
+const tanggal = `${d.getFullYear()}-${d.getMonth() + 1 < 10 ? "0" + (d.getMonth() + 1) : d.getMonth() + 1}-${d.getDate() < 10 ? "0" + d.getDate() : d.getDate()} ${d.getHours() < 10 ? "0" + d.getHours() : d.getHours()}:${d.getMinutes() < 10 ? "0" + d.getMinutes() : d.getMinutes()}:${d.getSeconds() < 10 ? "0" + d.getSeconds() : d.getSeconds()}`
 
 const template = `---
 judul: "${judul}"
 layout: "../../layout/post.astro"
 ringkasan: ""
 kategori: ""
-tanggal: "2023-02-01 09:01:00"
+tanggal: "${tanggal}"
 gambar: ""
 ---
 
 `
 
-// writeFileSync(`./src/pages/post/${slug(judul)}.mdx`, `halo`)
+writeFileSync(`./src/pages/post/${slug(judul)}.mdx`, template)
