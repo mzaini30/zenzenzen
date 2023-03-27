554f2785559fd6eec2ffeaca643f18db4c63abb4
mzaini30 <muhzaini30@gmail.com>
2023-03-28 06:26:42 +0800

    Menu

--- src/bagian/header.svelte
+++ src/bagian/header.svelte
@@ -1,6 +1,9 @@
 <script>
   import Cetak from "../fungsi/cetak.svelte";
   import semua_tulisan from "../fungsi/semua-tulisan";
+  import { goto } from "$app/navigation";
+
+  let kategori_target;
 
   const semua_postingan = semua_tulisan;
   // const semua_postingan = import.meta.glob("../postingan/*.mdx", {
@@ -11,6 +14,14 @@
     semua_kategori = [...semua_kategori, x[1].metadata.kategori];
   }
   semua_kategori = [...new Set(semua_kategori)].sort();
+
+  function capitalizeFirstLetter(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  async function menuju(lokasi) {
+    await goto(lokasi);
+  }
 </script>
 
 <div
@@ -18,6 +29,16 @@
 >
   <div class="mx-auto w-270 max-w-[95%] flex justify-between">
     <h1><a href="/">Zen Zen</a></h1>
+    <select
+      on:change={(event) =>
+        menuju(`/kategori/${event.target.value.toLowerCase()}`)}
+      class="text-black px-2 sm:hidden"
+    >
+      <option disabled selected="selected">Menu</option>
+      {#each semua_kategori as kategori}
+        <option>{capitalizeFirstLetter(kategori)}</option>
+      {/each}
+    </select>
     <div class="sm:flex gap-3 hidden">
       {#each semua_kategori as kategori}
         <a href="/kategori/{kategori}">{kategori}</a>
