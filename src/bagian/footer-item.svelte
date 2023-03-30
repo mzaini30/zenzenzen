c1a629664cf51a88e15537083dc2666ae2c950d7
mzaini30 <muhzaini30@gmail.com>
2023-03-30 10:00:59 +0800

    Upload dulu bagian footer

---
+++ src/bagian/footer-item.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let data;
+  export let judul;
+</script>
+
+<div>
+  <div class="uppercase">{judul}</div>
+  <ol class="text-justify">
+    {#each data.sort((a, b) => (a.teks > b.teks ? 1 : -1)).splice(0, 5) as x}
+      <li><a href={x.link}>{x.teks}</a></li>
+    {/each}
+  </ol>
+</div>
--- src/bagian/footer.svelte
+++ src/bagian/footer.svelte
@@ -1,3 +1,17 @@
+<script>
+  import addons_firefox from "../portfolio/addons-firefox";
+  import aplikasi_android from "../portfolio/aplikasi-android";
+  import github from "../portfolio/github";
+  import workflow_automa from "../portfolio/workflow-automa";
+  import Item from "./footer-item.svelte";
+</script>
+
 <div class="w-full p-3 text-sm bg-red-500 text-white text-center">
-  Aku harap, kamu suka dengan blog ini...
+  <div class="uppercase">Berikut ini adalah portfolioku</div>
+  <div class="gap-3 grid grid-cols-2 sm:grid-cols-4">
+    <Item judul="Addons Firefox" data={addons_firefox} />
+    <Item judul="Apliasi Android" data={aplikasi_android} />
+    <Item judul="Github" data={github} />
+    <Item judul="Workflow Automa" data={workflow_automa} />
+  </div>
 </div>
