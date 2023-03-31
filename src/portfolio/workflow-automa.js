0305ccc916d741046743751b649aa858ac476ae8
Zen <muhzaini30@gmail.com>
2023-03-31 11:55:01 +0800

    Update workflow-automa.js

--- src/portfolio/workflow-automa.js
+++ src/portfolio/workflow-automa.js
@@ -1,1 +1,16 @@
-2023-03-31 11:54:31
+export default [{
+    teks: 'Update Portfolio',
+    link: 'https://www.automa.site/workflow/P4NkdhAqXDZvaMmzdAOaJ',
+},{
+    teks: 'screenshot saham di pluang',
+    link: 'https://www.automa.site/workflow/Geutq1gY9YLXfVvpEcCjd',
+},{
+    teks: 'screenshot crypto di pluang',
+    link: 'https://www.automa.site/workflow/4x65Nl9og6sj21tE7A4Xa',
+},{
+    teks: 'Notifikasi Cek Crypto Signals',
+    link: 'https://www.automa.site/workflow/n-mc9NwYxgxFDB_lcCyPj',
+},{
+    teks: 'Crypto Signals',
+    link: 'https://www.automa.site/workflow/FPSoWF87YX5SeYyhAmIUN',
+}]
