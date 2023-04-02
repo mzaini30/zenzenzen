5c50e0525d355f756797793c68ac66e05c5a7ac2
mzaini30 <muhzaini30@gmail.com>
2023-04-02 23:41:29 +0800

    Portfolio selesai

--- src/bagian/footer-item.svelte
+++ src/bagian/footer-item.svelte
@@ -4,12 +4,16 @@
   export let link_selengkapnya;
 </script>
 
-<div>
+<div class="[&_*]:mb-1 last:[&_*]:mb-1">
   <div class="uppercase text-left font-bold">{judul}</div>
   <ul class="text-left ml-5">
-    {#each data.sort((a, b) => (a.teks > b.teks ? 1 : -1)).splice(0, 5) as x}
-      <li class="list-disc"><a href={x.link}>{x.teks}</a></li>
+    {#each data.sort((a, b) => (a.teks > b.teks ? 1 : -1)).slice(0, 5) as x}
+      <li class="list-disc capitalize">
+        <a class="hover:underline" href={x.link}>{x.teks}</a>
+      </li>
     {/each}
   </ul>
-  <p class="text-left"><a href={link_selengkapnya}>Selengkapnya &rarr;</a></p>
+  <p class="text-left">
+    <a class="hover:underline" href={link_selengkapnya}>Selengkapnya &rarr;</a>
+  </p>
 </div>
--- src/bagian/footer.svelte
+++ src/bagian/footer.svelte
@@ -6,27 +6,30 @@
   import Item from "./footer-item.svelte";
 </script>
 
-<div class="w-full p-3 text-sm bg-red-500 text-white text-center">
-  <div class="gap-4 grid grid-cols-2 sm:grid-cols-4">
-    <Item
-      judul="Addons Firefox"
-      data={addons_firefox}
-      link_selengkapnya="https://addons.mozilla.org/id/firefox/user/17443701/"
-    />
-    <Item
-      judul="Aplikasi Android"
-      data={aplikasi_android}
-      link_selengkapnya="https://play.google.com/store/apps/dev?id=5401138465689796048"
-    />
-    <Item
-      judul="Github"
-      data={github}
-      link_selengkapnya="https://github.com/mzaini30"
-    />
-    <Item
-      judul="Workflow Automa"
-      data={workflow_automa}
-      link_selengkapnya="https://www.automa.site/marketplace?author=mzaini30&workflowsBy=community"
-    />
+<div class="w-full p-3 text-sm bg-red-500 text-white  sm:pb-20">
+  <div class=" mx-auto w-270 max-w-[95%]">
+    <h2 class="text-3xl sm:text-[100px] my-5 sm:my-20">Portfolio.</h2>
+    <div class="gap-4 grid grid-cols-2 sm:grid-cols-4">
+      <Item
+        judul="Addons Firefox"
+        data={addons_firefox}
+        link_selengkapnya="https://addons.mozilla.org/id/firefox/user/17443701/"
+      />
+      <Item
+        judul="Aplikasi Android"
+        data={aplikasi_android}
+        link_selengkapnya="https://play.google.com/store/apps/dev?id=5401138465689796048"
+      />
+      <Item
+        judul="Github"
+        data={github}
+        link_selengkapnya="https://github.com/mzaini30"
+      />
+      <Item
+        judul="Workflow Automa"
+        data={workflow_automa}
+        link_selengkapnya="https://www.automa.site/marketplace?author=mzaini30&workflowsBy=community"
+      />
+    </div>
   </div>
 </div>
