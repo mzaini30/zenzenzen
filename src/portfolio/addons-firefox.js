63d109909db97de0856b947e0bf3a3fa5ab054de
Zen <muhzaini30@gmail.com>
2023-04-02 23:46:02 +0800

    Update addons-firefox.js

--- src/portfolio/addons-firefox.js
+++ src/portfolio/addons-firefox.js
@@ -1,1 +1,28 @@
-2023-04-02 11:45:31
+export default [{
+    teks: 'Writing',
+    link: 'https://addons.mozilla.org/en-US/firefox/addon/writing-simple/',
+},{
+    teks: 'UUID Generator',
+    link: 'https://addons.mozilla.org/en-US/firefox/addon/uuid-generator-2023/',
+},{
+    teks: 'Organize Jobs',
+    link: 'https://addons.mozilla.org/en-US/firefox/addon/organize-jobs/',
+},{
+    teks: 'Kalkulator Investasi',
+    link: 'https://addons.mozilla.org/en-US/firefox/addon/kalkulator-investasi/',
+},{
+    teks: 'TODO',
+    link: 'https://addons.mozilla.org/en-US/firefox/addon/todo-simple/',
+},{
+    teks: 'Multiple Choice Checker',
+    link: 'https://addons.mozilla.org/en-US/firefox/addon/multiple-choice-checker/',
+},{
+    teks: 'Get Ayat',
+    link: 'https://addons.mozilla.org/en-US/firefox/addon/get-ayat/',
+},{
+    teks: 'Licence Generator',
+    link: 'https://addons.mozilla.org/en-US/firefox/addon/licence-generator/',
+},{
+    teks: 'Conventional Commit',
+    link: 'https://addons.mozilla.org/en-US/firefox/addon/conventional-commit/',
+}]
