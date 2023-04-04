04fa7114642312c80fe352f3349eadf93e61c64d
Zen <muhzaini30@gmail.com>
2023-04-05 07:34:13 +0800

    Update github.js

--- src/portfolio/github.js
+++ src/portfolio/github.js
@@ -1,1 +1,19 @@
-2023-04-05 07:33:43
+export default [{
+    teks: 'svelte',
+    link: 'https://github.com/mzaini30/svelte',
+},{
+    teks: 'pg',
+    link: 'https://github.com/mzaini30/pg',
+},{
+    teks: 'zenia',
+    link: 'https://github.com/mzaini30/zenia',
+},{
+    teks: 'andro',
+    link: 'https://github.com/mzaini30/andro',
+},{
+    teks: 'bumi',
+    link: 'https://github.com/mzaini30/bumi',
+},{
+    teks: 'gajah',
+    link: 'https://github.com/mzaini30/gajah',
+}]
