662ce1c98a68f2ffa314e27a45dd07ee6686771c
mzaini30 <muhzaini30@gmail.com>
2023-04-04 22:47:07 +0800

    Modif beranda

--- src/bagian/terbaru.svelte
+++ src/bagian/terbaru.svelte
@@ -17,7 +17,7 @@
     return tulisan_rapi.filter((x) => x[1].metadata.kategori == kategori);
   }
   let pemrograman_pertama = filter_kategori("pemrograman")[0];
-  let pemrograman_lima_pertama = filter_kategori("pemrograman").slice(0, 5);
+  let pemrograman_lima_pertama = filter_kategori("pemrograman").slice(0, 7);
 </script>
 
 <JudulSegmen teks="Postingan Terbaru" link_selengkapnya="/semua-postingan" />
@@ -29,7 +29,7 @@
   />
 </div>
 <div class="grid grid-cols-1 gap-3 sm:grid-cols-2">
-  {#each tulisan_rapi.slice(0, 5) as x, n}
+  {#each tulisan_rapi.slice(0, 9) as x, n}
     {#if n != 0}
       <CardBersebelahan
         gambar={x[1].metadata.gambar}
@@ -39,26 +39,6 @@
     {/if}
   {/each}
 </div>
-<!-- <div class="grid grid-cols-2 gap-6 pt-3 mb-10">
-  {#each tulisan_rapi.slice(0, 7) as x}
-    <CardBiasa
-      link={x[0]}
-      gambar={x[1].metadata.gambar}
-      judul={x[1].metadata.judul}
-      kategori={x[1].metadata.kategori}
-    />
-  {/each}
-  <a href="https://wa.me/6281545143654">
-    <img
-      src={iklan}
-      alt="Iklan dulu ya"
-      title="Iklan dulu ya"
-      class="w-full aspect-video object-cover rounded"
-      width="460"
-      height="280"
-    />
-  </a>
-</div> -->
 <JudulSegmen teks="Pemrograman" link_selengkapnya="/kategori/pemrograman" />
 <div class="pt-3 ">
   <CardBesar
