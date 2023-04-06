a46b0ff826e44817023ec61e65f6247f7ef58aa0
Zen <muhzaini30@gmail.com>
2023-04-06 10:39:53 +0800

    Update addons-firefox.js

--- src/portfolio/addons-firefox.js
+++ src/portfolio/addons-firefox.js
@@ -1,1 +1,31 @@
-2023-04-06 10:39:20
+export default [{
+    teks: 'Writing',
+    link: 'https://addons.mozilla.org/en-US/firefox/addon/writing-simple/',
+},{
+    teks: 'Writing Arabic',
+    link: 'https://addons.mozilla.org/en-US/firefox/addon/writing-arabic/',
+},{
+    teks: 'UUID Generator',
+    link: 'https://addons.mozilla.org/en-US/firefox/addon/uuid-generator-2023/',
+},{
+    teks: 'Get Ayat',
+    link: 'https://addons.mozilla.org/en-US/firefox/addon/get-ayat/',
+},{
+    teks: 'Organize Jobs',
+    link: 'https://addons.mozilla.org/en-US/firefox/addon/organize-jobs/',
+},{
+    teks: 'Kalkulator Investasi',
+    link: 'https://addons.mozilla.org/en-US/firefox/addon/kalkulator-investasi/',
+},{
+    teks: 'Memilih Crypto',
+    link: 'https://addons.mozilla.org/en-US/firefox/addon/memilih-crypto/',
+},{
+    teks: 'Multiple Choice Checker',
+    link: 'https://addons.mozilla.org/en-US/firefox/addon/multiple-choice-checker/',
+},{
+    teks: 'Licence Generator',
+    link: 'https://addons.mozilla.org/en-US/firefox/addon/licence-generator/',
+},{
+    teks: 'TODO',
+    link: 'https://addons.mozilla.org/en-US/firefox/addon/todo-simple/',
+}]
