1b2e16959aff62f36e7cb22037cc72b413df6445
Zen <muhzaini30@gmail.com>
2023-04-06 10:40:25 +0800

    Update workflow-automa.js

--- src/portfolio/workflow-automa.js
+++ src/portfolio/workflow-automa.js
@@ -1,1 +1,25 @@
-2023-04-06 10:39:52
+export default [{
+    teks: 'Auto Buy Banyak Crypto di Pluang',
+    link: 'https://www.automa.site/workflow/EvbpOiQTritJEWIMGIW44',
+},{
+    teks: 'Daftarkan Satu Link Acak ke Google Search Console',
+    link: 'https://www.automa.site/workflow/9LHi6CILiNC_2CmXSv4XT',
+},{
+    teks: 'Ubah Privacy Policy dengan Cepat',
+    link: 'https://www.automa.site/workflow/EBBfCvJB1cJcVmmE0SQYp',
+},{
+    teks: 'Update Portfolio',
+    link: 'https://www.automa.site/workflow/P4NkdhAqXDZvaMmzdAOaJ',
+},{
+    teks: 'screenshot saham di pluang',
+    link: 'https://www.automa.site/workflow/Geutq1gY9YLXfVvpEcCjd',
+},{
+    teks: 'screenshot crypto di pluang',
+    link: 'https://www.automa.site/workflow/4x65Nl9og6sj21tE7A4Xa',
+},{
+    teks: 'Notifikasi Cek Crypto Signals',
+    link: 'https://www.automa.site/workflow/n-mc9NwYxgxFDB_lcCyPj',
+},{
+    teks: 'Crypto Signals',
+    link: 'https://www.automa.site/workflow/FPSoWF87YX5SeYyhAmIUN',
+}]
