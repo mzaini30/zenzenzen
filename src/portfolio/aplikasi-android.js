a36e14532704bcac25a13cf4ffa96362504eb7e2
Zen <muhzaini30@gmail.com>
2023-04-11 07:53:30 +0800

    Update aplikasi-android.js

--- src/portfolio/aplikasi-android.js
+++ src/portfolio/aplikasi-android.js
@@ -1,1 +1,31 @@
-2023-04-11 07:52:55
+export default [{
+    teks: 'KALIGRAFI',
+    link: 'https://play.google.com/store/apps/details?id=com.trihandayani.kaligrafi',
+},{
+    teks: 'Timbangan Pasar',
+    link: 'https://play.google.com/store/apps/details?id=com.mzaini30.timbanganpasar',
+},{
+    teks: 'TES PSIKOPAT',
+    link: 'https://play.google.com/store/apps/details?id=com.mzaini30.tespsikopat',
+},{
+    teks: 'Sholawat Jawa',
+    link: 'https://play.google.com/store/apps/details?id=com.sholawat.jawa',
+},{
+    teks: 'KALKULATOR TOKO',
+    link: 'https://play.google.com/store/apps/details?id=com.mzaini30.kalkulatortoko',
+},{
+    teks: 'JODOH',
+    link: 'https://play.google.com/store/apps/details?id=com.mzaini30.jodoh',
+},{
+    teks: 'SKORING PANAHAN',
+    link: 'https://play.google.com/store/apps/details?id=com.mzaini30.skoringpanahan',
+},{
+    teks: 'BELAJAR PEMROGRAMAN OFFLINE',
+    link: 'https://play.google.com/store/apps/details?id=com.mzaini30.belajarpemrogramanoffline',
+},{
+    teks: 'DIAGNOSA PSIKOLOGIS',
+    link: 'https://play.google.com/store/apps/details?id=com.mzaini30.diagnosapsikologis',
+},{
+    teks: 'Renungan',
+    link: 'https://play.google.com/store/apps/details?id=com.kisahzen.renungan',
+}]
