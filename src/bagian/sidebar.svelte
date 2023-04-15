dea9820546cb206f1fd8c2164b1e186d3408acf1
mzaini30 <muhzaini30@gmail.com>
2023-04-15 08:18:00 +0800

    Gambar di sidebar full width

--- src/bagian/sidebar.svelte
+++ src/bagian/sidebar.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div
-  class="last:[&>*]:sticky last:[&>*]:top-19 last:[&>*]:mb-0 [&>*]:mb-5 [&>*]:block"
+  class="last:[&>*]:sticky [&_img]:w-full last:[&>*]:top-19 last:[&>*]:mb-0 [&>*]:mb-5 [&>*]:block"
 >
   <a href="https://my.domainesia.com/ref.php?u=8297"
     ><img
@@ -24,17 +24,13 @@
       height="280"
     />
   </a> -->
-  <a
-    href="https://pluang.com/crypto-currency/trading-view-chart?cryptoCurrencySymbol=BTC"
-  >
-    <img
-      src={pluang}
-      alt="Kode referal Pluang"
-      title="Kode referal Pluang"
-      width="500"
-      height="896"
-    />
-  </a>
+  <img
+    src={pluang}
+    alt="Kode referal Pluang"
+    title="Kode referal Pluang"
+    width="500"
+    height="896"
+  />
   <img
     src="https://user-images.githubusercontent.com/7939342/229728577-e6309762-dd6e-4c8a-9ec6-162d3e3a27d8.jpeg"
     alt="Kode referal Pintu"
