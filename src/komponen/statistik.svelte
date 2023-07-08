dd7343f94a1878ee8049eefedf0cc0d96796b70f
mzaini30 <muhzaini30@gmail.com>
2023-07-09 07:20:37 +0800

    Upload dari laptop

--- src/komponen/statistik.svelte
+++ src/komponen/statistik.svelte
@@ -1,58 +1,7 @@
 <script>
-  export let judul;
+  export let judul
 
-  import excaliburZen from "excalibur-zen";
-  import { sql, id } from "../sql/statistik";
+  import stat_aja from 'stat-aja'
 
-  let tanggalan = new Date();
-  let bulan = tanggalan.getMonth() + 1;
-  let tahun = tanggalan.getFullYear();
-
-  let banyaknya;
-
-  async function init() {
-    banyaknya = await excaliburZen(sql, {
-      id,
-      kunci: "cek",
-      judul,
-      bulan,
-      tahun,
-    });
-    banyaknya = await banyaknya.json();
-    if (!banyaknya) {
-      // belum ada
-      let tambah_baru = await excaliburZen(sql, {
-        id,
-        kunci: "tambah-baru",
-        judul,
-        banyak: "1",
-        bulan,
-        tahun,
-      });
-    } else {
-      // sudah ada
-      let update_aja = await excaliburZen(sql, {
-        id,
-        kunci: "update-dulu",
-        banyak: +banyaknya[0].banyak + 1,
-        judul,
-        bulan,
-        tahun,
-      });
-    }
-  }
-  init();
-
-  async function cek() {
-    let terbaru = await excaliburZen(sql, {
-      id,
-      kunci: "ambil-terbanyak",
-      bulan,
-      tahun,
-    });
-    terbaru = await terbaru.json();
-    terbaru = terbaru.sort((b, a) => +a.banyak - +b.banyak);
-    console.log(JSON.stringify(terbaru, null, 2));
-  }
-  cek();
+  stat_aja({judul, tipe: 'Blog Zen Zen'})
 </script>
